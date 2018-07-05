e19a502bd473dca647fc5c7259670abab3574a0f
Joe Amedeo <joseph_amedeo@yahoo.com>
2018-07-05 16:49:46 -0400

    maybe one day I will make this app work

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.get('/', function(req, res) {
 	mongoClient.connect(connectionString, function(error, database){
 		if (error) throw error;
 		var finalResult = {'sensor1': null, 'sensor2': null};
+		var senor1 = [];
+		var sensor2 = [];
 		var dbo = database.db('SensorData');
 		var query17 = { gpio_number: '17'};
 		var query18 = { gpio_number: '18'};
@@ -20,13 +22,15 @@ app.get('/', function(req, res) {
 		dbo.collection('Data').find(query17).sort(sortObject).limit(1).toArray(function(err, result){
 			if (err) throw err;
 			console.log(result);
-			finalResult.sensor1.push(result);
+			sensor1.push(result);
 		});
 		dbo.collection('Data').find(query18).sort(sortObject).limit(1).toArray(function(err, result){
 			if (err) throw err;
 			console.log(result);
-			finalResult.sensor2.push(result);
+			sensor2.push(result);
 		});
+		finalResult.sensor1 = sensor1;
+		finalResult.sensor2 = sensor2;
 		console.log(finalResult);
 		res.json(finalResult);
 		database.close();
