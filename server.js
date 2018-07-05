2fc8bfdebd11ab45a2a4162b1274fa7fec92d03f
Joe Amedeo <joseph_amedeo@yahoo.com>
2018-07-05 07:47:30 -0400

    once again

--- server.js
+++ server.js
@@ -20,12 +20,12 @@ app.get('/', function(req, res) {
 		dbo.collection('Data').find(query17).sort(sortObject).limit(1).toArray(function(err, result){
 			if (err) throw err;
 			console.log(result);
-			finalResult.sensor1 = result;
+			finalResult.sensor1.push(result);
 		});
 		dbo.collection('Data').find(query18).sort(sortObject).limit(1).toArray(function(err, result){
 			if (err) throw err;
 			console.log(result);
-			finalResult.sensor2 = result
+			finalResult.sensor2.push(result);
 		});
 		console.log(finalResult);
 		res.json(finalResult);
@@ -36,7 +36,7 @@ app.get('/', function(req, res) {
 app.get('/:returnCount', function(req, res) {
 	mongoClient.connect(connectionString, function(error, database){
 		if (error) throw error;
-		var finalResult = {'sensor1': null, 'sensor2': null};
+		var finalResult = {'sensor1': [], 'sensor2': []};
 		var dbo = database.db('SensorData');
 		var query17 = { gpio_number: '17'};
 		var query18 = { gpio_number: '18'};
@@ -44,12 +44,12 @@ app.get('/:returnCount', function(req, res) {
 		var result17 = dbo.collection('Data').find(query17).sort(sortObject).limit(parseInt(req.params.returnCount)).toArray(function(err, result){
 			if (err) throw err;
 			console.log(result);
-			finalResult.sensor1 = result;
+			finalResult.sensor1.push(result);
 		});
 		var result18 = dbo.collection('Data').find(query18).sort(sortObject).limit(parseInt(req.params.returnCount)).toArray(function(err, result){
 			if (err) throw err;
 			console.log(result);
-			finalResult.sensor2 = result;
+			finalResult.sensor2.push(result);
 		});
 		console.log(finalResult);
 		res.json(finalResult);
