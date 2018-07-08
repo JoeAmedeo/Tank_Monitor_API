6902aaece44541e5ac3e883231c513a3a2118cde
Joe Amedeo <joseph_amedeo@yahoo.com>
2018-07-08 12:08:57 -0400

    remade second get request into post request, and added comments

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ var connectionString = process.env.MONGO_CONNECTION_STRING;
 
 var app = express();
 
+// We only need to parse jsons in this api, so we will only use 1 of 3 bodyParser functions
 app.use(bodyParser.json());
 
+// GET: will return all entries in the database when called
 app.get('/', function(req, res) {
 	mongoClient.connect(connectionString, function(error, database){
 		if (error) throw error;
@@ -22,13 +24,19 @@ app.get('/', function(req, res) {
 	})
 });
 
-app.get('/:sensorNumber/:rowCount', function(req, res) {
+/*
+ * POST: will take in a JSON with two parameters, sensorNumber and rowCount
+ * The query will find all database entries with the given sensor number.
+ * This result will be ordered by date (most recent to least recent entry)
+ * and the row count will limit how many rows will be sent back in the response.
+ */
+app.post('/', function(req, res) {
 	mongoClient.connect(connectionString, function(error, database){
 		if (error) throw error;
 		var dbo = database.db('SensorData');
-		var query = { gpio_number: req.params.sensorNumber};
+		var query = { gpio_number: req.body.sensorNumber};
 		var sortObject = { time: -1 };
-		dbo.collection('Data').find(query).sort(sortObject).limit(parseInt(req.params.rowCount)).toArray(function(err, result){
+		dbo.collection('Data').find(query).sort(sortObject).limit(parseInt(req.body.rowCount)).toArray(function(err, result){
 			if (err) throw err;
 			console.log(result);
 			res.json(result);
@@ -38,6 +46,7 @@ app.get('/:sensorNumber/:rowCount', function(req, res) {
 	})
 });
 
+// PUT: will insert the given JSON into the database when called
 app.put('/', function(req, res){
 	mongoClient.connect(connectionString, function(error, database){
 		if (error) throw error;
